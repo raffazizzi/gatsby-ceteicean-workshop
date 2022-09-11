644e7733f704b44d152a4e25218bda55dcc88397
raffazizzi <raffaeleviglianti@gmail.com>
2022-09-11 14:31:47 +0100

    New 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,12 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import Layout from "../components/layout"
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
+const NotFound = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout location="404">
+      <h2>404: Not found</h2>
+    </Layout>
   )
 }
 
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export default NotFound
